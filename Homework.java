d52ed69b99fffbd019a5300ebfb4793ea7804cad
ian.rosenberg <isr5@njit.edu>
2020-09-09 22:39:54 -0400

    Week 2 Homework

---
+++ Homework.java
@@ -0,0 +1,16 @@
+public class Homework {
+	public static void main(String[] args){
+		String name = "Ian Rosenberg";
+		
+		System.out.println("Hello, " + name);
+		
+		
+		int age = 26;
+		
+		System.out.println("Age is " + age);
+		
+		float floatVal = 1.23f;
+		
+		System.out.println("A decimal/floating point value: " + floatVal);
+	}
+}
