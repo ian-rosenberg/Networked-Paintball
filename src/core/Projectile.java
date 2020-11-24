51a72184ffb6714a61249327746698475d0a98d9
ian.rosenberg <isr5@njit.edu>
2020-11-24 16:09:54 -0500

    Setting up projectile class for real this time, closes #14

--- src/core/Projectile.java
+++ src/core/Projectile.java
@@ -1,10 +1,29 @@
 package core;
 
 import java.awt.Dimension;
+import java.util.List;
+
+import client.Player;
+import server.ClientPlayer;
 
 public class Projectile extends GameObject {
 	private Dimension screenBounds;
-	private float speed = 20f;
-	private int team;
-	private int radius;
+	private int radius = 5;
+	private Player owner = null;
+
+	private void checkScreenBounds() {
+
+	}
+
+	private void getCollidingPlayers(List<ClientPlayer> clientPlayers) {
+
+	}
+
+	protected void destroy() {
+
+	}
+
+	public void update() {
+
+	}
 }
